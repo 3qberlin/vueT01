4b94325846864dfa50ec6def88465f6c151676e7
Berlin <3q.berlin@gmail.com>
2024-02-02 10:12:56 +0800

    test: 🧪 test

---
+++ src/views/AboutView.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="about">
+    <h1>This is an about page??</h1>
+    {{ data }}
+  </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      data: {}
+    }
+  },
+  mounted() {
+    const url = import.meta.env.VITE_PATH;
+    const text = import.meta.env.VITE_TEXT;
+    const product = import.meta.env.VITE_PROD;
+    this.$http.get(url).then((res) => {
+      console.log(res)
+      console.log('text',text);
+      console.log('product',product);
+      this.data = res.data.info.seed
+    })
+  }
+}
+</script>
